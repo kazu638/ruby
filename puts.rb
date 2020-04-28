71b0659947dc4f9707b60055effdf3ff61fdc311
kazutaka <k89ut3903@gmail.com>
2020-04-28 19:17:03 +0900

    add_each

--- puts.rb
+++ puts.rb
@@ -25,3 +25,31 @@ price = 1980
 price2 = 5900
 
 puts price + price2
+
+users = ["satou", "tanaka", "yamada", "suzuki", "yanagi", "obata"]
+
+# !付けないと元データに反映されない。
+users.shuffle!
+
+team_a = []
+team_b = []
+
+
+users.each.with_index do |user, i|
+  if i.odd?
+    team_a << user  
+  else
+    team_b << user
+  end
+end
+
+text = "チームA\n"
+team_a.each do |user|
+  text += "#{user}さん\n"
+end
+text += "\nチームB\n"
+team_b.each do |user|
+  text += "#{user}さん\n"
+end
+
+puts text
