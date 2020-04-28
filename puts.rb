efdb9eaa111a28445099bd06fa9c2c40a6f7a177
kazutaka <k89ut3903@gmail.com>
2020-04-28 18:39:20 +0900

    add_array

--- puts.rb
+++ puts.rb
@@ -25,3 +25,26 @@ price = 1980
 price2 = 5900
 
 puts price + price2
+
+# 配列定義
+users = ["tanaka", "satou", "suzuki", "yamada"]
+
+puts users[2]
+
+users.each do |user|
+  puts "#{user}さん"
+end
+
+puts "-------------------------------"
+
+users.each { |user| puts "#{user}さん"}
+
+puts "-------------------------------"
+
+users.each.with_index(1) do |user, i|
+  puts "会員No.#{i} #{user}さん"
+end
+
+puts "-------------------------------"
+
+puts users.join("と、")
